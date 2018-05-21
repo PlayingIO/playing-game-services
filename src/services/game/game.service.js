31281afb9ff311444209cec901981552ce5fc489
forafish <fora.luo@gmail.com>
2018-05-21 19:25:41 +0800

    Fix get data property of object

--- src/services/game/game.service.js
+++ src/services/game/game.service.js
@@ -29,7 +29,7 @@ export class GameService extends Service {
   reorder (id, data, params, original) {
     return this.get(data.target).then((target) => {
       if (!target) throw new Error("data.target not exists");
-      target = target.data || target;
+      target = fp.propOf('data', target);
       return helpers.reorderPosition(this.Model, original, target.position, { classify: 'parent' });
     });
   }

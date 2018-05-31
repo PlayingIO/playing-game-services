81744db90e29fbf8f442b069186d1716693be399
forafish <fora.luo@gmail.com>
2018-05-31 18:10:41 +0800

    use playing-game-common

--- src/services/game/game.hooks.js
+++ src/services/game/game.hooks.js
@@ -3,8 +3,7 @@ import { associateCurrentUser, queryWithCurrentUser } from 'feathers-authenticat
 import { hooks } from 'mostly-feathers-mongoose';
 import { cache } from 'mostly-feathers-cache';
 import contents from 'playing-content-common';
-
-import GameEntity from '../../entities/game.entity';
+import { GameEntity } from 'playing-game-common';
 
 export default function (options = {}) {
   return {
